7fc843cf0a57717a98810c22d0806440d859b1e8
AnantShah001 <shahanant04@gmail.com>
2026-01-21 11:24:47 +0530

    SetUp TriggerBoxZone

--- Source/UE5_GameplaySystems/ShapeComponent/TriggerBoxZone.cpp
+++ Source/UE5_GameplaySystems/ShapeComponent/TriggerBoxZone.cpp
@@ -1,5 +1,23 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "ShapeComponent/TriggerBoxZone.h"
+#include "TriggerBoxZone.h"
+
+//Construction
+UTriggerBoxZone::UTriggerBoxZone()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+	UE_LOG(LogTemp, Warning, TEXT("UTriggerBoxZone : Construction"));
+}
+
+void UTriggerBoxZone::BeginPlay()
+{
+	UE_LOG(LogTemp, Warning, TEXT("UTriggerBoxZone : BeginPlay"));
+}
+
+void UTriggerBoxZone::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	UE_LOG(LogTemp, Warning, TEXT("UTriggerBoxZone : Tick"));
+}
+
 
--- Source/UE5_GameplaySystems/ShapeComponent/TriggerBoxZone.h
+++ Source/UE5_GameplaySystems/ShapeComponent/TriggerBoxZone.h
@@ -9,9 +9,18 @@
 /**
  * 
  */
-UCLASS()
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent)) //Show in UE_Editor
 class UE5_GAMEPLAYSYSTEMS_API UTriggerBoxZone : public UBoxComponent
 {
 	GENERATED_BODY()
-	
+
+public:
+	// Construction
+	UTriggerBoxZone();
+
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+protected:
+	virtual void BeginPlay() override ;
+
 };
