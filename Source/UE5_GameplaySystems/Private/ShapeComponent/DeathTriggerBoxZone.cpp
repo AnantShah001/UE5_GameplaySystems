0da9bb5956a84ac70e1b9a2df4b6ff01123b6100
AnantShah001 <shahanant04@gmail.com>
2026-02-13 22:45:23 +0530

    SetUp DeathTriggerBoxZone

--- Source/UE5_GameplaySystems/Private/ShapeComponent/DeathTriggerBoxZone.cpp
+++ Source/UE5_GameplaySystems/Private/ShapeComponent/DeathTriggerBoxZone.cpp
@@ -2,4 +2,23 @@
 
 
 #include "ShapeComponent/DeathTriggerBoxZone.h"
+#include "Kismet/GameplayStatics.h"
 
+UDeathTriggerBoxZone::UDeathTriggerBoxZone()
+{
+	UE_LOG(LogTemp, Warning, TEXT("DeathTriggerBoxZone"));
+}
+
+void UDeathTriggerBoxZone::BeginPlay()
+{
+	Super::BeginPlay();
+
+	OnComponentBeginOverlap.AddDynamic(this, &UDeathTriggerBoxZone::OnOverlapBegin);
+
+}
+
+void UDeathTriggerBoxZone::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Destroy Player Name : %s"), *OtherActor->GetName());
+
+}
--- Source/UE5_GameplaySystems/Public/ShapeComponent/DeathTriggerBoxZone.h
+++ Source/UE5_GameplaySystems/Public/ShapeComponent/DeathTriggerBoxZone.h
@@ -9,9 +9,17 @@
 /**
  * 
  */
-UCLASS()
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class UE5_GAMEPLAYSYSTEMS_API UDeathTriggerBoxZone : public UBoxComponent
 {
 	GENERATED_BODY()
-	
+
+public:
+	UDeathTriggerBoxZone();
+
+protected:
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 };
