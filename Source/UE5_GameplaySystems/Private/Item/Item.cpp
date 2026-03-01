396948158a221226f6389a6cf2b14af4b0b9f1c0
AnantShah001 <shahanant04@gmail.com>
2026-03-01 17:36:36 +0530

    Item

--- Source/UE5_GameplaySystems/Private/Item/Item.cpp
+++ Source/UE5_GameplaySystems/Private/Item/Item.cpp
@@ -2,6 +2,9 @@
 
 
 #include "Item/Item.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/SphereComponent.h"
+#include "UE5_GameplaySystems/UE5_GameplaySystemsCharacter.h"
 
 // Sets default values
 AItem::AItem()
@@ -9,6 +12,12 @@ AItem::AItem()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	Mesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	RootComponent = Mesh;
+
+	CollisionComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereCollision"));
+	CollisionComp->SetupAttachment(RootComponent);
 }
 
 // Called when the game starts or when spawned
@@ -16,6 +25,7 @@ void AItem::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	CollisionComp->OnComponentBeginOverlap.AddDynamic(this, &AItem::OnOverlapBegin);
 }
 
 // Called every frame
@@ -25,3 +35,10 @@ void AItem::Tick(float DeltaTime)
 
 }
 
+void AItem::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (AUE5_GameplaySystemsCharacter* PlayerCharacter = Cast<AUE5_GameplaySystemsCharacter>(OtherActor))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("On_Overlap_Begin : Item"));
+	}
+}
--- Source/UE5_GameplaySystems/Public/Item/Item.h
+++ Source/UE5_GameplaySystems/Public/Item/Item.h
@@ -6,6 +6,9 @@
 #include "GameFramework/Actor.h"
 #include "Item.generated.h"
 
+class UStaticMeshComponent;
+class USphereComponent;
+
 UCLASS()
 class UE5_GAMEPLAYSYSTEMS_API AItem : public AActor
 {
@@ -18,6 +21,19 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	// StaticMeshComponent
+	UPROPERTY(VisibleDefaultsOnly, Category = "Item")
+	UStaticMeshComponent* Mesh;
+
+	// Sphere collision component
+	UPROPERTY(VisibleDefaultsOnly, Category = "Item")
+	USphereComponent* CollisionComp;
+
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	float CollisionRadius = 10.f;
 
 public:	
 	// Called every frame
